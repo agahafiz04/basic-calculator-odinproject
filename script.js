3a438278ea128ca2cd9bb86f3efbdee32ccd598c
agahafiz04 <agahafiz04@gmail.com>
2023-08-30 14:42:49 +0700

    Fully functional calculator
    Bugfixes
    1. Button calculator now working correctly
    2. Calculator wont calculate on several operation
    
    Added Feature
    1. Calculator now round decimal number
    2. Showing error when inserted by unusual behavior
    3. Add decimal button
    
    May add another feature on keyboard support

--- script.js
+++ script.js
@@ -12,63 +12,114 @@ let displayValue = null;
 let displayState = "";
 
 const displayEl = document.querySelector(".display-text");
+const displayElTwo = document.querySelector(".display-text-two");
 const buttonEl = document.querySelectorAll(".key button");
-
-buttonEl.disable = true;
+const buttonDecimal = document.querySelector(".decimal");
 
 buttonEl.forEach(function (buttonNumber) {
   buttonNumber.addEventListener("click", function () {
+    displayElTwo.textContent = "";
+    checkFunction(buttonNumber);
     displayFunction(buttonNumber);
     CalculateFunction(buttonNumber);
   });
 });
 
+// Check The State
+function checkFunction(buttonNumber) {
+  if (displayEl.textContent.includes(".")) {
+    buttonDecimal.disabled = true;
+  } else {
+    buttonDecimal.disabled = false;
+  }
+
+  if (result == NaN || result == undefined) {
+    displayElTwo.textContent = "Error, please AC";
+    clearDisplay();
+  }
+
+  const buttonValue = Array.from(buttonNumber.classList);
+
+  if (displayEl.textContent == "") {
+    if (buttonValue.includes("number")) {
+    } else if (buttonValue.includes("operand")) {
+      clearDisplay();
+      displayElTwo.textContent =
+        "Cant Press Operand On Initial State, Please Reset";
+    } else if (buttonValue.includes("clear")) {
+    } else if (buttonValue.includes("equals")) {
+      displayElTwo.textContent =
+        "Cant Press Equals On Initial State Please Reset";
+      clearDisplay();
+    }
+  }
+}
+
 // Calculate The Number
 function CalculateFunction(buttonNumber) {
   const buttonValue = Array.from(buttonNumber.classList);
 
-  if (result > 0) {
-    console.log("state two");
-    if (buttonValue.includes("number") && count == 0) {
-      console.log("state two number times");
-      tempNum = displayEl.textContent;
-      secondNumber = tempNum;
-    } else if (buttonValue.includes("operand") && count == 0) {
-      console.log("operand on state two");
-      count += 2;
-      operator = buttonValue[2];
-      console.log(`The operator is ${operator}, Go To Result`);
-    }
-  }
-
   if (result == 0) {
     console.log("state one");
     if (buttonValue.includes("number") && count == 0) {
+      console.log("state one if one");
       tempNum = displayEl.textContent;
       firstNumber = tempNum;
     } else if (buttonValue.includes("operand") && count == 0) {
+      console.log("state one if two");
       count++;
       operator = buttonValue[2];
       console.log(`The operator is ${operator}, Change To Second Number`);
     } else if (buttonValue.includes("number") && count == 1) {
+      console.log("state one if three");
       tempNum = displayEl.textContent;
       secondNumber = tempNum;
     } else if (buttonValue.includes("operand") && count == 1) {
+      console.log("state one if four");
       count++;
     }
   }
 
-  if (buttonValue.includes("equals") || count >= 2) {
-    console.log(`state equals`);
-    result = operate(firstNumber, secondNumber, operator);
-    displayEl.textContent = result;
-
-    count = 0;
-    tempNum = 0;
-    firstNumber = result;
-    secondNumber = 0;
-    operator = null;
+  if (result !== 0) {
+    console.log("state two");
+    if (buttonValue.includes("number") && count == 0) {
+      console.log("state two if one");
+      tempNum = displayEl.textContent;
+      secondNumber = tempNum;
+      result = operate(firstNumber, secondNumber, operator);
+    } else if (buttonValue.includes("operand") && count == 0) {
+      console.log("state two if two");
+      operator = buttonValue[2];
+      console.log(`Your operator is ${operator}`);
+      count += 2;
+    }
   }
+
+  if (buttonValue.includes("equals") || count >= 2) {
+    if (result == 0) {
+      console.log("state equals");
+      result = operate(firstNumber, secondNumber, operator);
+      displayEquals();
+
+      operator = buttonValue[2];
+      firstNumber = result;
+      secondNumber = 0;
+      tempNum = 0;
+      count = 0;
+    } else if (result !== 0) {
+      console.log("state equals");
+      displayEquals();
+
+      operator = buttonValue[2];
+      firstNumber = result;
+      secondNumber = 0;
+      tempNum = 0;
+      count = 0;
+    }
+  }
+
+  // 10 + 5 - 7 - 2 = 6
+  // 12 + 7 = 19, - 5 * 3 = 42 - 3 - 2 = 37
 
   console.log(`Your first number is ${firstNumber} and tempNum is ${tempNum}`);
   console.log(
@@ -76,6 +127,7 @@ function CalculateFunction(buttonNumber) {
   );
   console.log(`Your count number is ${count}`);
   console.log(`Your result is ${result}`);
+  console.log(`Your operator is ${operator}`);
 }
 
 // Display The Number
@@ -83,7 +135,7 @@ function displayFunction(buttonNumber) {
   displayValue = buttonNumber.textContent;
   const buttonValue = Array.from(buttonNumber.classList);
 
-  if (displayState == "Operand") {
+  if (displayState == "Operand" || displayState == "Equals") {
     displayState = "Number";
     displayEl.textContent = "";
   }
@@ -94,8 +146,6 @@ function displayFunction(buttonNumber) {
     displayOperand();
   } else if (buttonValue.includes("clear")) {
     clearDisplay();
-  } else if (buttonValue.includes("equals")) {
-    console.log("Equals Pressed");
   }
 
   console.log(displayState);
@@ -115,6 +165,11 @@ function displayOperand() {
   displayEl.textContent = `${displayValue}`;
 }
 
+function displayEquals() {
+  displayState = "Equals";
+  displayEl.textContent = `${result}`;
+}
+
 function clearDisplay() {
   displayEl.textContent = "";
   console.clear();
@@ -125,6 +180,7 @@ function clearDisplay() {
   operator = null;
   count = 0;
   result = 0;
+  tempNum = 0;
 
   displayValue = null;
   displayState = "";
@@ -144,25 +200,29 @@ function operate(numOne, numTwo, operate) {
 }
 
 function add(numOne, numTwo) {
-  return Number(numOne) + Number(numTwo);
+  let total = Number(numOne) + Number(numTwo);
+  return Number(total.toFixed(5));
 }
 
 function subtract(numOne, numTwo) {
-  return Number(numOne) - Number(numTwo);
+  let total = Number(numOne) - Number(numTwo);
+  return Number(total.toFixed(5));
 }
 
 function multiply(numOne, numTwo) {
-  return Number(numOne) * Number(numTwo);
+  let total = Number(numOne) * Number(numTwo);
+  return Number(total.toFixed(5));
 }
 
 function divide(numOne, numTwo) {
-  return Number(numOne) / Number(numTwo);
+  if (numTwo == 0 || numTwo == "0") {
+    displayElTwo.textContent = "Infinity, Enter Correct Calculation";
+    return (result = 0);
+  }
+  let total = Number(numOne) / Number(numTwo);
+  return Number(total.toFixed(5));
 }
 
-// Tomorrow Task
-// 3. Your calculator should not evaluate more than a single pair of numbers at a time.
-
-// 4. Pressing = before entering all of the numbers or an operator could cause problems!
-// 5. Pressing “clear” should wipe out any existing data.. make sure the user is really starting fresh after pressing “clear” (Complete the algorithm)
-// 6. Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
-// 7. Should round answers with long decimals so that they don’t overflow the screen.
+// Tommorow Task :)
+// 1. Add Keyboard Support
+// 2. Add Backspace Support
